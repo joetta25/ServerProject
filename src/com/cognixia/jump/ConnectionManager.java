849d3fcf25e64a168280f149dd52b8f93a2ffac0
ThomasOcean <ThomasOceanMartinez@gmail.com>
2020-06-25 15:55:05 -0400

    I just copied the ConnectionManager in

---
+++ src/com/cognixia/jump/ConnectionManager.java
@@ -0,0 +1,41 @@
+package com.cognixia.jump;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+	
+	private static final String URL = "jdbc:mysql://localhost:3306/sakila"; // ?serverTimezone=EST5EDT
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "root"; // Windows: root, Mac/Linux: Root@123
+	
+	private static Connection conn;
+	
+	private static void makeConnection() {
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static Connection getConnection() {
+		
+		if(conn == null) {
+			makeConnection();
+		}
+		
+		return conn;
+	}
+
+}
