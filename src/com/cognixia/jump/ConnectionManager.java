e2956ac7711d8ebaa5bb690e7677ee1e7dd5a2c6
joetta25 <joetta.chasidy@gmail.com>
2020-06-26 12:01:55 -0400

    commit

---
+++ src/com/cognixia/jump/ConnectionManager.java
@@ -0,0 +1,31 @@
+package com.cognixia.jump;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class cd {
+
+	public static Connection getConnection () throws SQLException {
+        System.out.println("\n\n***** MySQL JDBC Connection Testing *****");
+		   Connection conn = null;
+        try
+        {
+        	 
+            String userName = "root";
+            String password = "root123#";
+            String url = "jdbc:mysql://localhost:3306/sakila?serverTimezone=EST5EDT";        
+            conn = DriverManager.getConnection (url, userName, password);
+            System.out.println ("\nDatabase Connection Established...");
+        }
+       catch (SQLException ex)
+        {
+    	   
+		       System.err.println ("Cannot connect to database server");
+			   ex.printStackTrace();
+        }      
+		   
+        
+		return conn;
+    }
+	
+}
